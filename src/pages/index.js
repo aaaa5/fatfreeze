ea9542d3ed1ce8fdaa60f5bf488f6ec247a0001a
Andriy Statkevych <astudio777@gmail.com>
2020-01-30 19:24:02 +0200

    modified:   src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,7 +122,7 @@ const IndexPage = () => (
         </div>
         <div className={"forming"}>
             <div className={"container"}>
-                <form method="post" action="#" netlify-honeypot="bot-field" data-netlify="true">
+                <form method="post" netlify-honeypot="bot-field" data-netlify="true">
                 
                 <label>
                     Email
