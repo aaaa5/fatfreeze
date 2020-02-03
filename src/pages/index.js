3896f4d09f319d7782bbb75478410e8702e277c0
Andriy Statkevych <astudio777@gmail.com>
2020-02-03 19:57:28 +0200

    modified:   src/pages/index.js 	deleted:    static/images/Bg.jpg 	deleted:    static/images/Bg_old.jpg

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import featureImage from "../../static/images/bg.jpg";
+import featureImage from "../../static/images/Bg.png";
 import thumbnailEvent from "../../static/images/feature-event.png"
 import thumbnailBoard from "../../static/images/feature-board.png"
 import thumbnailNews from "../../static/images/feature-news.png"
