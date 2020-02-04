fe77fb84f328b17c7b8f71c2ba7b968956f018de
Andriy Statkevych <astudio777@gmail.com>
2020-02-04 17:30:55 +0200

    modified:   gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
                 name: `gatsby-starter-default`,
                 short_name: `starter`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#FF54AC`,
+                background_color: `#1aafe7`,
+                theme_color: `#1aafe7`,
                 display: `minimal-ui`,
                 icon: `${__dirname}/static/images/histaff.png`, // This path is relative to the root of the site.
             },
