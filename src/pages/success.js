705af1d0e61a48aa3186d81a922ffbb52bbd8076
Andriy Statkevych <astudio777@gmail.com>
2020-02-10 21:38:41 +0200

    modified:   src/pages/success.js

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,12 +1,19 @@
 import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
-export default () => (
-  <div style={{
-    display: "flex",
-    height: "100vh",
-    alignItems: "center",
-    flexWrap: "wrap"
-  }}>
-    <h2 style={{ textAlign:"center", width:"100%" }}>Thank you for Booking!</h2>
-  </div>
+const SuccessPage = () => (
+    <Layout>
+        <SEO title="Success Page"/>
+        <div style={{
+          display: "flex",
+          height: "100vh",
+          alignItems: "center",
+          flexWrap: "wrap"
+        }}>
+        <h2 style={{ textAlign:"center", width:"100%" }}>Thank you for Booking!</h2>
+      </div>
+    </Layout>
 )
+
+export default SuccessPage
